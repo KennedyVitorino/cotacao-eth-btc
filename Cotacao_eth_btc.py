d43a45f308ae94c38263c3273566740545d1c8f4
Kennedy <dkennyvs@gmail.com>
2023-05-27 11:48:31 -0300

    Update project

--- Cotacao_eth_btc.py
+++ Cotacao_eth_btc.py
@@ -4,39 +4,37 @@ import requests
 
 
 def cotacao_eth_btc():
-    print('\n+------+-------+-------+-------+-------+ \n'
-          f'|    -> Cotação \033[1;94mBITCOIN\033[m para BRL <-    | \n'
-          f'+-------+-------+-------+-------+------+ \n'
-          f'|    -> Cotação \033[1;95mETHEREUM\033[m para BRL <-   | \n'
-          f'+------+-------+-------+-------+-------+ \n')
-
+    print('Cotação do BITCOIN e do ETHEREUM')
+    print()
+    
     cripto_moeda = input('BCT ou ETH?').strip()
 
     while True:
-        usdbrl = requests.get(url=f'http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL')
-        json_data_ = json.loads(usdbrl.text)
+        usd_brl = requests.get(url=f'https://economia.awesomeapi.com.br/json/last/USD'
+                                   f'-BRL,EUR-BRL')
+        json_data_ = json.loads(usd_brl.text)
 
-        btc_eth = requests.get(url=f'https://www.mercadobitcoin.net/api/{cripto_moeda}/ticker/')
+        btc_eth = requests.get(url=f'https://www.mercadobitcoin.net/api/{cripto_moeda}'
+                                   f'/ticker/')
         json_data_btc_eth = json.loads(btc_eth.text)
 
-        api_usdbrl = float(json_data_['USDBRL']['bid'])
-        api_eurbrl = float(json_data_['EURBRL']['bid'])
+        api_usd_brl = float(json_data_['USDBRL']['bid'])
+        api_eur_brl = float(json_data_['EURBRL']['bid'])
 
         api = float(json_data_btc_eth['ticker']['last'])
         api_low = float(json_data_btc_eth['ticker']['low'])
         api_high = float(json_data_btc_eth['ticker']['high'])
-        # sleep(0.5)
+        sleep(0.5)
 
-        print(f'Preço atual do dolar convertido em real US${api_usdbrl:.2f} \n'
-              f'Preço atual do euro convertido em real Є{api_eurbrl:.2f}')
+        print()
+        print(f'Dólar para BRL em tempo real R$ {api_usd_brl:.2f}')
+        print(f'Euro para BRL em tempo real R$ {api_eur_brl:.2f}')
 
-        print('+-----------+-----------+------------+ \n'
-              f'|                                    |'
-              f'\n| Cotação atual:...... R${api:.2f}   |\n'
-              f'| Menor preço:........ R${api_low:.2f}   |\n'
-              f'| Maior preço:........ R${api_high:.2f}   |\n'
-              f'|                                    |\n'
-              '+-----------+-----------+------------+')
+        print()
+        print('PREÇO ATUAL DO ETHERUM')
+        print(f'Cotação atual:...... R${api:.2f}')
+        print(f'Menor preço:........ R${api_low:.2f}')
+        print(f'Maior preço:........ R${api_high:.2f}')
         break
 
 
--- Robô Bitcoin, Ethereum.py
+++ Robô Bitcoin, Ethereum.py
@@ -9,7 +9,7 @@ print('\n+------+-------+-------+-------+-------+ \n'
       f'+------+-------+-------+-------+-------+ \n')
 
 # modalidade - 1 2 3
-criptomoeda = input('\033[1;93mBITCOIN\033[m ou \033[1;95mETHEREUM\033[m:').upper().strip()
+criptomoeda = input('BITCOIN ou ETHEREUM:').upper().strip()
 modalidade = input("""Escolha o que você deseja Monitorar
 (1) Venda | (2) Compra | (3) Venda e compra: """)
 
@@ -23,18 +23,21 @@ if modalidade == '3':
     venda = input(f'Por quanto você deseja vender o {criptomoeda}? Ex: {resultado} ')
 
     while True:
-        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}/ticker/')
+        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}'
+                                     f'/ticker/')
         json_data = json.loads(resultado.text)  # converter o resultado em texto
         resultado = json_data['ticker']['last']
-        print(f'\033[1;91m{resultado}\033[m')
-        sleep(2.5)
+        print(f'{resultado}')
+        sleep(1.5)
 
-        # quando o resultado for maior ou igual ao valor digitado ele enviará mensagem de vender
+        # quando o resultado for maior ou igual ao valor digitado ele enviará mensagem de
+        # vender
         if compra >= resultado:
             print('\n$$$ Hora de comprar ativos! $$$$')
             break
 
-        if venda <= resultado:  # quando a compra for maior ou igual ao valor digitado ele enviará mensagem de comprar
+        if venda <= resultado:  # quando a compra for maior ou igual ao valor digitado
+            # ele enviará mensagem de comprar
             print('\n$$$$ Hora de vender seus ativos $$$$')
             break
 
@@ -42,13 +45,15 @@ elif modalidade == '2':
     compra = input(f'\nPor quanto você deseja comprar o {criptomoeda}? Ex: {resultado} ')
 
     while True:
-        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}/ticker/')
+        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}'
+                                     f'/ticker/')
         json_data = json.loads(resultado.text)  # converter o resultado em texto
         resultado = json_data['ticker']['last']
-        print(f'\033[1;91m{resultado}\033[m')
-        sleep(2.5)
+        print(f'{resultado}')
+        sleep(1.5)
 
-        # quando o resultado for maior ou igual ao valor digitado ele enviará mensagem de vender
+        # quando o resultado for maior ou igual ao valor digitado ele enviará mensagem
+        # de vender
         if compra >= resultado:
             print(f'\nHora de vender seus ativos à {resultado}')  #
             break
@@ -56,18 +61,13 @@ else:
     venda = input(f'\nPor quanto você deseja seu vender o {criptomoeda} Ex: {resultado} ')
 
     while True:
-        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}/ticker/')
+        resultado = requests.get(url=f'https://www.mercadobitcoin.net/api/{criptomoeda}'
+                                     f'/ticker/')
         json_data = json.loads(resultado.text)  # converter o resultado em texto
         resultado = json_data['ticker']['last']
-        print(f'\033[1;91m{resultado}\033[m')
-        sleep(2.5)
+        print(f'{resultado}')
+        sleep(1.5)
 
         if venda <= resultado:
             print(f'\nHora de vender seus ativos à {resultado}')
             break
-
-
-""" Magreza, quando o resultado é menor que 18,5 kg/m2;
-Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
-Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
-Obesidade, quando o resultado é maior que 30 kg/m2."""
